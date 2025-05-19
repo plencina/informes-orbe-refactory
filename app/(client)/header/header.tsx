cde9e69f40dcb259b0e45a8040e12927a45a863a
plencina <patolencina5151@gmail.com>
2025-05-19 12:42:53 -0300

    alerta de actualización

--- app/(client)/header/header.tsx
+++ app/(client)/header/header.tsx
@@ -1,6 +1,7 @@
 import styles from "@/app/(client)/styles/header.module.css"
 export default function Header () {
     return <div className={styles.main}>
+        <p className={styles.alert}>🆕 Actualizando la web, las noticias pueden no estar actualizadas 🆕</p>
         <img 
         src="img1.jpg"
         alt="Alerta sobre el mosquito dengue"
